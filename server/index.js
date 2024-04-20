cbbca7bb1f7fa2d8f92e0212cd9abf0254831478
Nosang Subba <nosangdev@gmail.com>
2024-04-20 10:43:18 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const userRouter = require("./routes/user");
 const path = require("path");
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use("/admin", adminRouter)
